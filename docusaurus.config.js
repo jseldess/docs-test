73ce7ca710e56756aeed08003757311beac0b032
Jesse Seldess <jesse@pinecone.io>
2023-12-14 02:01:49 -0500

    More

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -164,8 +164,3 @@ const config = {
 };
 
 export default config;
-
-export default {
-  url: 'https://docusaurus-2.netlify.app', // Url to your site with no trailing slash
-  baseUrl: '/', // Base directory of your site relative to your repo
-};
