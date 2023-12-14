17eeef2b7f85e0bb3cb9e6a277ef0530cd4d32b9
Jesse Seldess <jesse@pinecone.io>
2023-12-14 09:33:19 -0500

    Remove build dir

--- build/assets/js/99786e1b.7cdc97bc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2589],{8819:e=>{e.exports=JSON.parse('{"title":"Getting Started","description":"Get started with Pinecone.","slug":"/category/getting-started-2","permalink":"/docs/./category/getting-started-2","navigation":{"next":{"title":"Overview","permalink":"/docs/./getting-started/overview"}}}')}}]);
--- build/assets/js/db58a77d.8f6e955b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[6067],{2730:e=>{e.exports=JSON.parse('{"title":"Getting Started","description":"Get started with Pinecone.","slug":"/category/getting-started","permalink":"/docs/legacy/category/getting-started","navigation":{"next":{"title":"Overview","permalink":"/docs/legacy/getting-started/overview"}}}')}}]);
